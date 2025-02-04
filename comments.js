e0474ce7e3128ccf28e1bee5cec3ab7080997f2b
scheppuri <scheppuri@commvault.com>
2025-02-04 22:47:26 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+//create web server
+//create route for /comments
+//send back some data
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var obj = {
+        name: 'John',
+        age: 30
+    };
+    res.end(JSON.stringify(obj));
+}).listen(1337, '
